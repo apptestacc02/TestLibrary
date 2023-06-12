2d28889cff1fbb849f90a6b6aaba27312102add5
apptestacc <apptestacc02@gmail.com>
2023-06-12 17:51:35 +0530

    version change

--- app/src/main/java/com/example/testlibrary/MainActivity.kt
+++ app/src/main/java/com/example/testlibrary/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.testlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mylibrary.app.ToastMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       // ToastMessage.toastMessage()
     }
 }
